5c602a8e2537acf3cac9a5e6590bfe24c8f0b3cb
tz <nadesiko7856@yahoo.co.jp>
2021-03-17 20:03:26 +0900

    正解不正解にアニメーションと仮SE,テキストUI仮置き

--- kc3_d/Assets/TZ/Scripts/AnswerBehaivour.cs
+++ kc3_d/Assets/TZ/Scripts/AnswerBehaivour.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class AnswerBehaivour : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+namespace kc3.d.tz.divide {
+    public class AnswerBehaivour : MonoBehaviour {
+        Transform myTransform;
+        SpriteRenderer spriteRenderer;
+        readonly Vector3 DISAPPERAR_SCALE= new Vector3(1.5f, 1.5f, 0);
+        readonly float DISAPPERAR_TIME = 0.7f;
+        readonly Color DISAPPERAR_COLOR = new Color(1, 1, 1, 0.7f);
+        void Start() {
+            myTransform = gameObject.GetComponent<Transform>();
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            myTransform.DOScale(DISAPPERAR_SCALE, DISAPPERAR_TIME);
+            spriteRenderer.DOColor(DISAPPERAR_COLOR, DISAPPERAR_TIME)
+                .OnComplete(DestroyObject);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void DestroyObject() {
+            Destroy(gameObject);
+        }
     }
 }
