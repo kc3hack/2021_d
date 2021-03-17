dc2981107eb5cbfe15f7fd6715eddadba835617b
tz <nadesiko7856@yahoo.co.jp>
2021-03-17 17:41:22 +0900

    羊生成関連、ボタン押しで次へ送る

---
+++ kc3_d/Assets/TZ/Scripts/WoolChange.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace kc3.d.tz.divide {
+    public enum WoolID {
+        FEMALE,
+        MALE,
+        NULL
+    }
+    public class WoolChange : MonoBehaviour {
+        SpriteRenderer myRenderer;
+        WoolID myID;
+        [SerializeField] WoolSpritesObject woolSpritesObject;
+        void Start() {
+            myRenderer = gameObject.GetComponent<SpriteRenderer>();
+            SetWool();
+        }
+
+        public void SetWool() {
+            int num = Random.Range(0, 2);
+            myID = (WoolID)num;
+            myRenderer.sprite = woolSpritesObject.GetSprite(myID);
+        }
+
+        public void ChangeWoolData(WoolID nextMyID) {
+            myID = nextMyID;
+            myRenderer.sprite = woolSpritesObject.GetSprite(myID);
+        }
+
+        public WoolID GetID() {
+            return myID;
+        }
+    }
+}
---
+++ kc3_d/Assets/TZ/Scripts/WoolMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kc3.d.tz.divide {
+    public class WoolMove : MonoBehaviour {
+        [SerializeField] WoolChange[] wools;
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        public void MoveWoolNext() {
+            WoolID beforeID = WoolID.NULL;
+            foreach(WoolChange wool in wools) {
+                if(beforeID == WoolID.NULL) {
+                    beforeID = wool.GetID();
+                    wool.SetWool();
+                } else {
+                    WoolID temp = wool.GetID();
+                    wool.ChangeWoolData(beforeID);
+                    beforeID = temp;
+                }
+            }
+        }
+    }
+}
