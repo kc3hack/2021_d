925fb62ce3f7559fdd402fa08db9a4af634a49f3
tz <nadesiko7856@yahoo.co.jp>
2021-03-18 16:38:03 +0900

    スコアとスコア倍率処理、表示

--- kc3_d/Assets/TZ/Scripts/AnsweCheck.cs
+++ kc3_d/Assets/TZ/Scripts/AnsweCheck.cs
@@ -7,11 +7,14 @@ namespace kc3.d.tz.divide{
         [SerializeField] WoolChange foremost;
         [SerializeField] AnswerApper answerApper;
         [SerializeField] WoolMove woolMove;
+        [SerializeField] ScoreManager scoreManager;
         public void OnClickFemaleButton() {
             if(foremost.GetID() == WoolID.FEMALE) {
                 answerApper.AppeearAns(WoolID.FEMALE, true);
+                scoreManager.Answerd(true);
             } else {
                 answerApper.AppeearAns(WoolID.FEMALE, false);
+                scoreManager.Answerd(false);
             }
             woolMove.MoveWoolNext();
         }
@@ -19,8 +22,10 @@ namespace kc3.d.tz.divide{
         public void OnClickMaleButton() {
             if (foremost.GetID() == WoolID.MALE) {
                 answerApper.AppeearAns(WoolID.MALE, true);
+                scoreManager.Answerd(true);
             } else {
                 answerApper.AppeearAns(WoolID.MALE, false);
+                scoreManager.Answerd(false);
             }
             woolMove.MoveWoolNext();
         }
---
+++ kc3_d/Assets/TZ/Scripts/ScoreManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+namespace kc3.d.tz.divide {
+    public class ScoreManager : MonoBehaviour {
+
+        [SerializeField] Text magnification, score;
+        int scoreNum, magNum;
+        readonly int BASE_SCORE = 100;
+        readonly int BASE_MAG = 1;
+        Color HIGH = new Color(0.88f, 0.047f,0.047f,1);
+        Color LOW = new Color(0, 0, 0, 1);
+        private void Start() {
+            magNum = BASE_MAG;
+        }
+        /// <summary>
+        /// 羊選択ボタンが押されたとき、正解不正解がわたされる。スコアや倍率の表示
+        /// </summary>
+        /// <param name="istrue"></param>
+        public void Answerd(bool istrue) {
+            int beforeScore = scoreNum;
+            if (istrue) {
+                scoreNum += BASE_SCORE * magNum;
+                magNum++;
+            } else {
+                scoreNum -= BASE_SCORE;
+                magNum = 1;
+            }
+            magnification.text = "x" + magNum.ToString();
+            CheckMagColor();
+            DOTween.To(() => beforeScore, (n) => beforeScore = n, scoreNum, 0.5f)
+                .OnUpdate(() => score.text = beforeScore.ToString());
+        }
+        /// <summary>
+        /// 倍率が高いとき色を赤くしてアピールする
+        /// </summary>
+        public void CheckMagColor() {
+            if(magNum >= 30) {
+                magnification.color = HIGH;
+            } else {
+                magnification.color = LOW;
+            }
+        }
+    }
+}
