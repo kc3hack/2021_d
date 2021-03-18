22979f11c0fa5badc73799f024cea12fb678bd4b
tz <nadesiko7856@yahoo.co.jp>
2021-03-18 17:12:41 +0900

    タイマーアニメーションとカウント　終了時遷移などは保留

---
+++ kc3_d/Assets/TZ/Scripts/TimerCount.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace kc3.d.tz.divide {
+    public class TimerCount : MonoBehaviour {
+        [SerializeField] Image timerFill;
+        [SerializeField] Text timerText;
+        [SerializeField] RectTransform timerTransform;
+        readonly int GAME_TIME = 60;
+        readonly Vector3 TIMER_ANIMATION_SCALE = new Vector3(1.3f, 1.3f, 1);
+
+        void Start() {
+            timerFill.DOFillAmount(1, GAME_TIME);
+            StartCoroutine(TextTimer());
+        }
+        /// <summary>
+        /// 十秒以後数字でのタイムカウントを行うコルーチン
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator  TextTimer() {
+            yield return new WaitForSeconds(50);
+            timerText.text = 10.ToString();
+            timerTransform.DOScale(TIMER_ANIMATION_SCALE, 0.5f).SetLoops(20);
+            for(int i = 9; i > -1; i--) {
+                yield return new WaitForSeconds(1);
+                timerText.text = i.ToString();
+            }
+        }
+    }
+}
