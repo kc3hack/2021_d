a42c511150f7992fffb6cab8bae7a2dc9eea2da5
NemuNemu-zzz <80695330+NemuNemu-zzz@users.noreply.github.com>
2021-03-21 06:57:54 +0900

    進化システム組み込み（演出の修正必要あり） ついでに超雑にツイート機能実装

---
+++ kc3_d/Assets/Nemu/Script/Home/UseTwitter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseTwitter : MonoBehaviour
+{
+    public void Tweet(){
+        Application.OpenURL($"https://twitter.com/intent/tweet?text=今起きた\n&hashtags=眠い");
+    }
+}
