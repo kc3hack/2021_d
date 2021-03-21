ab761ae2e2d6a693a9f85ff302b4330e51fa5dc0
NemuNemu-zzz <80695330+NemuNemu-zzz@users.noreply.github.com>
2021-03-21 11:43:58 +0900

    微修正OF微修正

---
+++ kc3_d/Assets/Nemu/Script/Home/SleepAlbum.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SleepAlbum")]
+public class SleepAlbum : ScriptableObject
+{
+    struct aaa
+    {
+        public int settime;
+        public int getuptime;
+        public int gamescore;
+        public Sprite woll;
+    }
+    Queue<string> aa;
+}
